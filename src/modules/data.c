77473a15062ae93210028c12d8b83ed7577f7c46
ymber <mtaylor1252dev@gmail.com>
2018-10-28 09:16:34 +0000

    astyle data.c

--- src/modules/data.c
+++ src/modules/data.c
@@ -19,75 +19,74 @@ double get_cpu_utilization(char *cpuN) {
     char *str = NULL;
     size_t len = 0;
     char *token;
-	char *stat_line;
-	FILE *fp = fopen("/proc/stat", "r");
-	while (getline(&str, &len, fp) != -1) {
-		stat_line = malloc(strlen(str));
-		strcpy(stat_line, str);
-		token = strtok(str, " ");
-		if (strcmp(token, cpuN) == 0) {
-			free(str);
-			fclose(fp);
-			break;
-		}
-		else if (token == NULL) {
-			fprintf(stderr, "no cpuid %s found in /proc/stat\n", cpuN);
-			fclose(fp);
-			free(str);
-		}
-	}
+    char *stat_line;
+    FILE *fp = fopen("/proc/stat", "r");
+    while (getline(&str, &len, fp) != -1) {
+        stat_line = malloc(strlen(str));
+        strcpy(stat_line, str);
+        token = strtok(str, " ");
+        if (strcmp(token, cpuN) == 0) {
+            free(str);
+            fclose(fp);
+            break;
+        }
+        else if (token == NULL) {
+            fprintf(stderr, "no cpuid %s found in /proc/stat\n", cpuN);
+            fclose(fp);
+            free(str);
+        }
+    }
 
-	token = strtok(stat_line, " ");
-	double idle = 0;
-	double working = 0;
-	int i = 0;
-	while (token != NULL) {
-		token = strtok(NULL, " ");
-		if (token == NULL) {
-			break;
-		}
-		// add idle and iowait figures to idle time
-		else if (i == 3 || i == 4) {
-			idle += strtol(token, NULL, 10);
-		}
-		// add all other figures to working time
-		else {
-			working += strtol(token, NULL, 10);
-		}
-		++i;
-	}
+    token = strtok(stat_line, " ");
+    double idle = 0;
+    double working = 0;
+    int i = 0;
+    while (token != NULL) {
+        token = strtok(NULL, " ");
+        if (token == NULL) {
+            break;
+        }
+        // add idle and iowait figures to idle time
+        else if (i == 3 || i == 4) {
+            idle += strtol(token, NULL, 10);
+        }
+        // add all other figures to working time
+        else {
+            working += strtol(token, NULL, 10);
+        }
+        ++i;
+    }
     free(stat_line);
-    
-	return working / (idle + working);
+
+    return working / (idle + working);
 }
 
 double get_memory_usage() {
-	char *str = NULL;
+    char *str = NULL;
     size_t len = 0;
-	FILE *fp = fopen("/proc/meminfo", "r");
+    FILE *fp = fopen("/proc/meminfo", "r");
 
-	// Stores values for MemTotal, MemFree, Buffers, and Cache
-	int mem_vals[4];
+    // Stores values for MemTotal, MemFree, Buffers, and Cache
+    int mem_vals[4];
 
-	int index = 0;
-	for (int i = 0; i < 5; ++i) {
-		// Don't store MemAvailable line
-		if (i == 2) {
-			getline(&str, &len, fp);
-			continue;
-		}
-		getline(&str, &len, fp);
-		strtok(str, " ");
-		mem_vals[index] = strtol(strtok(NULL, " "), NULL, 10);
-		printf("%d %d\n", index, mem_vals[index]);
-		++index;
-	}
+    int index = 0;
+    for (int i = 0; i < 5; ++i) {
+        // Don't store MemAvailable line
+        if (i == 2) {
+            getline(&str, &len, fp);
+            continue;
+        }
+        getline(&str, &len, fp);
+        strtok(str, " ");
+        mem_vals[index] = strtol(strtok(NULL, " "), NULL, 10);
+        ++index;
+    }
 
     fclose(fp);
     free(str);
 
-	double used_fraction = (double)(mem_vals[0] - mem_vals[1] - mem_vals[2] - mem_vals[3]) / (double)mem_vals[0];
-	printf("%f\n", used_fraction);
+    double used_fraction = (double)(mem_vals[0] - mem_vals[1] - mem_vals[2] - mem_vals[3]) / (double)mem_vals[0];
+    printf("%f\n", used_fraction);
 
-	return used_fraction;
+    return used_fraction;
 }
