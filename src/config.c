bffee080b8de9f437d25ab8a6f42e191d50a7622
ymber <mtaylor1252dev@gmail.com>
2018-11-01 13:42:48 +0000

    extern global config

---
+++ src/config.c
@@ -0,0 +1,25 @@
+#include <cairo.h>
+
+#include "config.h"
+#include "modules/status.h"
+#include "modules/background.h"
+#include "modules/data_curves.h"
+#include "modules/resource_charts.h"
+#include "modules/date_time.h"
+
+Settings_t settings = {
+    1024,
+    768,
+    1
+};
+
+void (*daemon_fncs[1])() = {
+    update_status
+};
+
+void (*display_fncs[4])(cairo_t*) = {
+    draw_background,
+    draw_rings,
+    draw_charts,
+    date_time
+};
--- src/config.h
+++ src/config.h
@@ -3,34 +3,15 @@
 
 #include <cairo.h>
 
-#include "overlay.h"
-#include "modules/status.h"
-#include "modules/background.h"
-#include "modules/data_curves.h"
-#include "modules/resource_charts.h"
-#include "modules/date_time.h"
-
 typedef struct {
     int size_x;
     int size_y;
-    double frequency;
+    double daemon_freq;
+    double display_freq;
 } Settings_t;
 
-Settings_t settings = {
-    1024,
-    768,
-    1
-};
-
-void (*daemon_fncs[1])() = {
-    update_status
-};
-
-void (*display_fncs[4])(cairo_t*) = {
-    draw_background,
-    draw_rings,
-    draw_charts,
-    date_time
-};
+extern Settings_t settings;
+extern void (*daemon_fncs[1])();
+extern void (*display_fncs[4])(cairo_t*);
 
 #endif
